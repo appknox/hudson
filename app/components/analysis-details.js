af1e0d1bad143621d308e02f3241a427cafe03ea
yashwin <yashwinpoojary@gmail.com>
2018-04-04 16:21:42 +0530

    enhance: decaffeinate analysis details

---
+++ app/components/analysis-details.js
@@ -0,0 +1,128 @@
+import Ember from 'ember';
+import ENUMS from 'hudson/enums';
+import { task } from 'ember-concurrency';
+
+const isEmpty = inputValue=> Ember.isEmpty(inputValue);
+
+const { get, set } = Ember;
+
+const AnalysisDetailsComponent = Ember.Component.extend({
+
+  findingId: 0,
+  findings: [],
+  findingTitle: "",
+  findingDescription: "",
+
+  scopes: ENUMS.SCOPE.CHOICES.slice(0, -1),
+  statuses: ENUMS.ANALYSIS_STATUS.CHOICES.slice(0),
+  owasps: ENUMS.OWASP_CATEGORIES.CHOICES.slice(0, -1),
+  attackVectors: ENUMS.ATTACK_VECTOR.CHOICES.slice(0, -1),
+  integrityImpacts: ENUMS.INTEGRITY_IMPACT.CHOICES.slice(0, -1),
+  userInteractions: ENUMS.USER_INTERACTION.CHOICES.slice(0, -1),
+  attackComplexities: ENUMS.ATTACK_COMPLEXITY.CHOICES.slice(0, -1),
+  requiredPrevileges: ENUMS.PRIVILEGES_REQUIRED.CHOICES.slice(0, -1),
+  availabilityImpacts: ENUMS.AVAILABILITY_IMPACT.CHOICES.slice(0, -1),
+  confidentialityImpacts: ENUMS.CONFIDENTIALITY_IMPACT.CHOICES.slice(0, -1),
+
+  allFindings: (function() {
+    let findingId = this.get("findingId");
+    const findings = this.get("findings");
+    const that = this;
+    findings.forEach(function(finding) {
+      findingId = findingId + 1;
+      finding.id = findingId;
+      that.set("findingId", findingId);
+    });
+    return findings;
+  }).property("findings"),
+
+  confirmCallback() {
+    const availableFindings = this.get("availableFindings");
+    this.set("findings", availableFindings);
+    return this.set("showRemoveFindingConfirmBox", false);
+  },
+
+  availableFindings: Ember.computed.filter('allFindings', function(allFinding) {
+    const deletedFinding = this.get("deletedFinding");
+    return allFinding.id !== deletedFinding;
+  }),
+
+  actions: {
+    selectScope(param) {
+      this.set('selectedScope', param);
+    },
+
+    selectStatus(param) {
+      this.set('selectedStatus', param);
+    },
+
+    selectAttackVector(param) {
+      this.set('selectedAttackVector', param);
+    },
+
+    selectOwaspCategory(param) {
+      this.set('selectedOwaspCategory', param);
+    },
+
+    selectIntegrityImpact(param) {
+      this.set('selectedIntegrityImpact', param);
+    },
+
+    selectUserInteraction(param) {
+      this.set('selectedUserInteraction', param);
+    },
+
+    selectAttackComplexity(param) {
+      this.set('selectedAttackComplexity', param);
+    },
+
+    selectRequiredPrevilege(param) {
+      this.set('selectedRequiredPrevilege', param);
+    },
+
+    selectAvailabilityImpact(param) {
+      this.set('selectedAvailabilityImpact', param);
+    },
+
+    selectConfidentialityImpact(param) {
+      this.set('selectedConfidentialityImpact', param);
+    },
+
+    addFinding() {
+      const findingTitle = this.get("findingTitle");
+      const findingDescription = this.get("findingDescription");
+      for (let inputValue of [findingTitle, findingDescription ]) {
+        if (isEmpty(inputValue)) { return this.get("notify").error("Please fill all the details"); }
+      }
+      let findingId = this.get("findingId");
+      findingId = findingId + 1;
+      const findings = this.get("findings");
+      const newFinding = {
+        id: findingId,
+        title: findingTitle,
+        description: findingDescription
+      };
+      findings.addObject(newFinding);
+      this.set("findingId", findingId);
+      this.set("findings", findings);
+      this.get("notify").success("Finding Added");
+      this.setProperties({
+        findingTitle: "",
+        findingDescription: ""
+      });
+    },
+
+    openRemoveFindingConfirmBox(param) {
+      this.set("deletedFinding", param);
+      this.set("showRemoveFindingConfirmBox", true);
+    },
+
+    openRemoveFileConfirmBox(param) {
+      this.set("deletedFile", param);
+      this.set("showRemoveFileConfirmBox", true);
+    }
+  }
+});
+
+
+export default AnalysisDetailsComponent;
