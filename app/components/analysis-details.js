c11cd494a80cc88b03de9f7bfff4cc6a342d3c61
yashwin <yashwinpoojary@gmail.com>
2018-04-04 18:19:13 +0530

    feat: upload attachments

--- app/components/analysis-details.js
+++ app/components/analysis-details.js
@@ -1,5 +1,8 @@
 import Ember from 'ember';
 import ENUMS from 'hudson/enums';
+import { task } from 'ember-concurrency';
+
+const { get, set } = Ember;
 
 const isEmpty = inputValue=> Ember.isEmpty(inputValue);
 
@@ -44,7 +47,35 @@ const AnalysisDetailsComponent = Ember.Component.extend({
     return allFinding.id !== deletedFinding;
   }),
 
+  uploadPhoto: task(function * (file) {
+    let product = this.modelFor('product');
+    let photo = this.store.createRecord('photo', {
+      product,
+      filename: get(file, 'name'),
+      filesize: get(file, 'size')
+    });
+
+    try {
+      file.readAsDataURL().then(function (url) {
+        if (get(photo, 'url') == null) {
+          set(photo, 'url', url);
+        }
+      });
+
+      let response = yield file.upload('/api/images/upload');
+      set(photo, 'url', response.headers.Location);
+      yield photo.save();
+    } catch (e) {
+      photo.rollback();
+    }
+  }).maxConcurrency(3).enqueue(),
+
   actions: {
+    
+    uploadImage(file) {
+      get(this, 'uploadPhoto').perform(file);
+    },
+
     selectScope(param) {
       this.set('selectedScope', param);
     },
