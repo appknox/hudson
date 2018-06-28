af5c99c2cb4fdd1e4dac0f747f9867dc2d826f6c
yashwin <yashwinpoojary@gmail.com>
2018-06-28 14:34:27 +0530

    get files

--- app/adapters/file.js
+++ app/adapters/file.js
@@ -2,13 +2,18 @@ import DS from 'ember-data';
 import ENV from 'hudson/config/environment';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
-const ProjectAdapter = DS.JSONAPIAdapter.extend(DataAdapterMixin, {
+const FileAdapter = DS.JSONAPIAdapter.extend(DataAdapterMixin, {
   authorizer: 'authorizer:hudson',
   host: ENV.host,
   namespace: "hudson-api",
   headers: {
     'Accept': 'application/json'
+  },
+  query: function query(store, type, q) {
+    q.id = 5;
+    let url = `${this.get('host')}/${this.get('namespace')}/projects/${q.id}/files`;
+    return this.ajax(url, 'GET');
   }
 });
 
-export default ProjectAdapter;
+export default FileAdapter;
