12a386899b09fda3bd2f452a833f586ae6235ec8
yashwin <yashwinpoojary@gmail.com>
2018-07-11 14:01:10 +0530

    feat: download app from file list

--- app/components/file-search-overview.js
+++ app/components/file-search-overview.js
@@ -1,7 +1,26 @@
 import Ember from 'ember';
+import ENV from 'hudson/config/environment';
 
 const FileSearchOverviewComponent = Ember.Component.extend({
 
-  tagName: ['tr']});
+  tagName: ['tr'],
+
+  actions: {
+    downloadApp() {
+      const fileId = this.get("file.id");
+      const data = {
+        file_id: fileId,
+        submit: "Download"
+      };
+      return this.get("ajax").post(ENV.endpoints.downloadApp, {data})
+      .then(() => {
+        this.get("notify").success("App Downloaded Successfully");
+      }, () => {
+        this.get("notify").error("Sorry something went wrong, please try again");
+      })
+    }
+  }
+
+});
 
 export default FileSearchOverviewComponent;
